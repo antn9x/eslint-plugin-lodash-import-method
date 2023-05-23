b374e20f0eaef5e49f0c6253b447d450fd36ce78
Tạ Nhật An <antn@ftech.ai>
2023-05-23 17:05:06 +0700

    init with rule

---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-lodash-import-method
+
+import lodash submodule only
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-lodash-import-method`:
+
+```sh
+npm install eslint-plugin-lodash-import-method --save-dev
+```
+
+## Usage
+
+Add `lodash-import-method` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "lodash-import-method"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "lodash-import-method/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/lodash-import-method.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview lodash-import-method
+ * @author antn9x
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/lodash-import-method"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("lodash-import-method", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+    {
+      code: "import min from 'lodash/min'",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+      parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'module'
+      }
+    },
+  ],
+
+  invalid: [
+    {
+      code: "import {min} from 'lodash'",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+      parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'module'
+      }
+    },
+  ],
+});
